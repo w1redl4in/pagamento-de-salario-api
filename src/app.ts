b2a94bfebb549d66feac6200e222624834f1b69b
Felipe <felipe.araujo@hubfintech.com.br>
2021-06-21 16:55:47 -0300

    feat: app :sparkles:

---
+++ src/app.ts
@@ -0,0 +1,72 @@
+import { Namespace, createNamespace } from 'continuation-local-storage';
+import logger from '@middlewares/logger';
+import morgan from 'morgan-body';
+import cors from 'cors';
+import express, {
+  Application,
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from 'express';
+import { ErrorHandler } from 'express-handler-errors';
+
+class App {
+  public readonly app: Application;
+
+  private readonly session: Namespace;
+
+  constructor() {
+    this.app = express();
+    this.session = createNamespace('session');
+    this.middlewares();
+    this.errorHandle();
+  }
+
+  private middlewares(): void {
+    this.app.use(express.json());
+    this.app.use(cors());
+
+    const reqId = require('express-request-id');
+
+    this.app.use(reqId());
+
+    const attachContext: RequestHandler = (
+      _: Request,
+      __: Response,
+      next: NextFunction
+    ) => {
+      this.session.run(() => next());
+    };
+
+    const setRequestId: RequestHandler = (
+      req: any,
+      _: Response,
+      next: NextFunction
+    ) => {
+      this.session.set('id', req.id);
+      next();
+    };
+
+    this.app.use(attachContext, setRequestId);
+
+    morgan(this.app, {
+      noColors: true,
+      prettify: false,
+      logReqUserAgent: false,
+      stream: {
+        write: (msg: string) => logger.info(msg) as any,
+      },
+    });
+  }
+
+  private errorHandle(): void {
+    this.app.use(
+      (err: Error, _: Request, res: Response, next: NextFunction) => {
+        new ErrorHandler().handle(err, res, next, logger as any);
+      }
+    );
+  }
+}
+
+export default new App();
