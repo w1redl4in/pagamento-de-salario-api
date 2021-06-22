d72e56d2c2102db635fcc8245c439d3cd5ced32c
Felipe <felipe.araujo@hubfintech.com.br>
2021-06-22 13:07:14 -0300

    feat: receiving a single file :sparkles:

--- src/apps/File/routes.ts
+++ src/apps/File/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import 'express-async-errors';
+
+import multer from 'multer';
+
 import * as FileController from './FileController';
 
 const route = Router();
 
-route.post('/', FileController.create);
+const upload = multer();
+
+route.post('/', upload.single('file'), FileController.create);
 
 export default route;
