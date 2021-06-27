da4d262f86bbfb90626823f668ccfd8fae73f25d
Felipe <felipe.araujo@hubfintech.com.br>
2021-06-27 14:01:05 -0300

    feat: publishing every line on rabbit queue :sparkles:

--- src/apps/File/FileService.ts
+++ src/apps/File/FileService.ts
@@ -1,11 +1,14 @@
-import { dbConnections } from '@config/index';
+import { dbConnections, rabbit } from '@config/index';
 import { CustomError } from 'express-handler-errors';
 import { getConnection, MongoRepository } from 'typeorm';
 import { ObjectId } from 'bson';
+import { RabbitPublisher } from 'src/services/RabbitPublisher';
 import { File, fileStatusEnum } from './File.entity';
 
 class FileService {
   private readonly repository: MongoRepository<File>;
+
+  private publisher = RabbitPublisher.getInstance();
 
   constructor() {
     this.repository = getConnection(
@@ -16,23 +19,28 @@ class FileService {
   private async parseFile(
     file: Express.Multer.File,
     _id: ObjectId
-  ): Promise<File> {
+  ): Promise<void> {
     try {
       const array = file.buffer.toString().split(/\n/);
       const line = array.map((item) => item.split(';'));
 
-      const updatedFile = await this.repository.save({
-        _id,
-        fileStatus: fileStatusEnum.PARSED,
-        items: line.map((i: string[]) => ({
-          document: i[0],
-          name: i[1],
-          cep: i[2],
-          email: i[3],
-        })),
-      });
+      line.map(async (l) => {
+        await this.publisher.publishOnQueue(
+          rabbit.validator_document_queue,
+          JSON.stringify(l)
+        );
 
-      return updatedFile;
+        await this.repository.save({
+          _id,
+          fileStatus: fileStatusEnum.PARSED,
+          items: line.map((i: string[]) => ({
+            document: i[0],
+            name: i[1],
+            cep: i[2],
+            email: i[3],
+          })),
+        });
+      });
     } catch (error) {
       throw new CustomError({
         code: 'FILE_GENERIC_ERROR',
@@ -42,7 +50,7 @@ class FileService {
     }
   }
 
-  async create(file: Express.Multer.File): Promise<File> {
+  async create(file: Express.Multer.File): Promise<void> {
     try {
       const _id = new ObjectId();
 
